f828128bbdf429e4b675448187ea8555add4abdf
danimohamed <mohameddani993@gmail.com>
2024-01-10 18:40:33 +0100

    Prepare your web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,15 @@
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '/server_name _;/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx restart
